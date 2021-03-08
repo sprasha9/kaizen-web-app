672f356ce572604cb0023b4a71248445de5ac278
sprasha9 <mail.prashanthsekar@gmail.com>
2021-03-08 16:06:59 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/com/kaizen/web/KaizenWebApplication.java
@@ -0,0 +1,13 @@
+package com.kaizen.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaizenWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaizenWebApplication.class, args);
+	}
+
+}
